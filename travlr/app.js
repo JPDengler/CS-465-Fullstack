96b64bebf4c4884962cf82fb9567efe66b0af0e3
JPDengler <jp.dengler@gmail.com>
2024-11-17 08:43:20 -0500

    TRAVEL/ROOMS/MEALS Dynamic Json setup

--- travlr/app.js
+++ travlr/app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var mealsRouter = require('./app_server/routes/meals'); // Add meals route
+var roomsRouter = require('./app_server/routes/rooms'); // Add rooms route
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/meals', mealsRouter); // Register the meals route
+app.use('/rooms', roomsRouter); // Register the rooms route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
