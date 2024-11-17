9d54a91e793e337fa3a7d402299e87e44f40bd94
JPDengler <jp.dengler@gmail.com>
2024-11-17 08:31:57 -0500

    meals and travel working HBS

--- travlr/app.js
+++ travlr/app.js
@@ -8,7 +8,8 @@ const hbs = require('hbs');
 // Update the paths to point to the routes in the app_server directory
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel'); // Add the travel route
+var travelRouter = require('./app_server/routes/travel');
+var mealsRouter = require('./app_server/routes/meals'); // Add meals route
 
 var app = express();
 
@@ -28,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/travel', travelRouter); // Register the travel route
+app.use('/travel', travelRouter);
+app.use('/meals', mealsRouter); // Register the meals route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,5 +49,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
